abe89b83b2f92b63e71d855000e7d35dc082cf54
Sébastien Serre <sserre.bx@gmail.com>
2016-08-06 01:38:53 +0200

    with json

--- src/main/java/io/sysmo/hooks/template/HelloHook.java
+++ src/main/java/io/sysmo/hooks/template/HelloHook.java
@@ -1,8 +1,15 @@
 package io.sysmo.hooks.template;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.json.Json;
+import javax.json.JsonWriter;
+import javax.json.JsonObjectBuilder;
+
 
 public class HelloHook extends HttpServlet {
 
@@ -10,15 +17,22 @@ public class HelloHook extends HttpServlet {
             HttpServletRequest request,
             HttpServletResponse response) throws IOException, ServletException
     {
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Hello World!</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<h1>Hello World!</h1>");
-        out.println("</body>");
-        out.println("</html>");
+
+        JsonObjectBuilder object = Json.createObjectBuilder();
+
+        object.add("hello", "0");
+        object.add("hello1", "b");
+        object.add("hello2", "c");
+        object.add("hello3", "d");
+        object.add("hello4", "e");
+
+        response.setContentType("application/json");
+        response.setHeader("Pragma", "no-cache"); // for HTTP 1.0
+        response.setHeader("Cache-Control",
+               "private, no-store, no-cache, must-revalidate"); // for HTTP 1.1
+
+        JsonWriter writer = Json.createWriter(response.getWriter());
+        writer.writeObject(object.build());
+
     }
 }
